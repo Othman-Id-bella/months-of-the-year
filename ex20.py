21870ff74989d983f4b3e3628dd2a412ceca6833
Othman Bella <147095415+Othman-Id-bella@users.noreply.github.com>
2023-11-16 00:30:18 +0100

    Add files via upload

---
+++ ex20.py
@@ -0,0 +1,28 @@
+M=input("enter the name of the month:")
+
+if M=="January":
+    print("the number of days of the month is",31)
+elif M=="February":
+    print("the number of days of the month is",28,"or",29)
+elif M=="March":
+    print("the number of days of the month is",31)
+elif M=="April":
+    print("the number of days of the month is",30)
+elif M=="May":
+    print("the number of days of the month is",31)
+elif M=="June":
+    print("the number of days of the month is",30)
+elif M=="July":
+    print("the number of days of the month is",31)
+elif M=="August":
+    print("the number of days of the month is",31)
+elif M=="September":
+    print("the number of days of the month is",30)
+elif M=="October":
+    print("the number of days of the month is",31)
+elif M=="November":
+    print("the number of days of the month is",30)
+elif M=="December":
+    print("the number of days of the month is",31)
+else:
+    print("the name of this month does not exist")
